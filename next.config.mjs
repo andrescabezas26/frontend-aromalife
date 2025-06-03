2979fc659766f38d26964867c84d6209bfd3388c
Davide Flamini Cazaran <davidecazaran@gmail.com>
2025-06-03 15:04:07 -0500

    cambios despliegue

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // Increase to 10MB (adjust as needed)
+    },
+  },
+};
 
-export default nextConfig
+export default nextConfig;
